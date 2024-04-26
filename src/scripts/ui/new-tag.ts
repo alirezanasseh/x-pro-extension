7b57ed99fe11cfe044f22215498e00911f9d8500
alireza <alireza.nasseh@gmail.com>
2024-04-27 00:09:52 +0200

    use ui elements, add autocomplete

--- src/scripts/ui/new-tag.ts
+++ src/scripts/ui/new-tag.ts
@@ -1,21 +1,36 @@
+import { Div } from "./elements/div.element";
+import { Input } from "./elements/input.element";
+import { Button } from "./elements/button.element";
+import { AutoCompleteTag } from "../functions/auto-complete-tag";
+
 export interface INewTag {
   newTagEl: HTMLDivElement;
   inputEl: HTMLInputElement;
 }
 
 export function NewTag(clickFunc: (newTag: INewTag) => void): INewTag {
-  const newTagEl = document.createElement("div");
-  newTagEl.className = "new-tag";
-  const inputEl = document.createElement("input");
-  inputEl.placeholder = "New tag";
-  inputEl.className = "vazirmatn";
-  const buttonEl = document.createElement("button");
-  buttonEl.id = "add-tag";
-  buttonEl.innerText = "✔";
-  buttonEl.className = "vazirmatn";
-  buttonEl.title = "Save";
-  buttonEl.onclick = () => clickFunc({ newTagEl, inputEl });
+  const newTagEl = Div({
+    className: "new-tag",
+    id: "new-tag",
+  });
+  const inputEl = Input({
+    className: "vazirmatn tag-input",
+    placeholder: "New tag",
+    onkeyup: AutoCompleteTag,
+  });
+  const buttonEl = Button({
+    id: "add-tag",
+    innerText: "✔",
+    className: "vazirmatn",
+    title: "Save",
+    onclick: () => clickFunc({ newTagEl, inputEl }),
+  });
+  const autoCompleteListEl = Div({
+    className: "auto-complete-list",
+    id: "auto-complete-list",
+  });
   newTagEl.appendChild(inputEl);
   newTagEl.appendChild(buttonEl);
+  newTagEl.appendChild(autoCompleteListEl);
   return { newTagEl, inputEl };
 }
